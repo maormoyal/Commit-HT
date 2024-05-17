c1fa8c6927a63f8d7693129107637923fc2411b5
maor moyal <maorkab@gmail.com>
2024-05-18 00:04:24 +0300

    split the app

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule, getConnectionToken } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import * as env from 'dotenv';
 
 import { AppController } from './app.controller';
@@ -9,29 +7,10 @@ import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { Connection } from 'mongoose';
 
-const rootPath = join(
-  __dirname,
-  '..',
-  '..',
-  '..',
-  '..',
-  'dist',
-  'apps',
-  'client'
-);
-console.log('Resolved Path:', rootPath);
-
 env.config();
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(process.env.DB_URI),
-    UserModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
-      exclude: ['/api*'],
-    }),
-  ],
+  imports: [MongooseModule.forRoot(process.env.DB_URI), UserModule],
   controllers: [AppController],
   providers: [
     AppService,
